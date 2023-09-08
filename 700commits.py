56d2a84a90406f923912d2034a250b253e3f49f4
Sameep Dhakal <sameep.dhakal721108@gmail.com>
2023-09-08 20:16:54 +0545

    Commit 1: Add new line

---
+++ 700commits.py
@@ -0,0 +1,34 @@
+import os
+import subprocess
+import time
+
+# Replace these with your repository information
+repository_path = "/home/sameeps/Desktop/Test-repo"
+# Relative to the repository root
+file_path = "/home/sameeps/Desktop/Test-repo/700commits.txt"
+
+# Change to the repository directory
+os.chdir(repository_path)
+
+# Loop to create commits
+for i in range(45, 699):
+    # Read the current content of the file
+    with open(file_path, "r") as file:
+        content = file.read()
+
+    # Add a new line of text for each commit
+    new_line = f"Commit {i+1}: New line of text\n"
+    new_content = content + new_line
+
+    # Write the new content to the file
+    with open(file_path, "w") as file:
+        file.write(new_content)
+
+    # Stage and commit the changes using Git
+    subprocess.run(["git", "add", file_path])
+    subprocess.run(["git", "commit", "-m", f"Commit {i+1}: Add new line"])
+
+    time.sleep(5)
+
+print("Commits created successfully.")
+# Commit 1: New line of text
